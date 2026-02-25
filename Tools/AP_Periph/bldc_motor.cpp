08cbfa495defa1a67d08b2d2192f9f7023236353
Adin Kojic <adin.kojic@stratospheres.net>
2026-02-25 10:11:05 -0500

    GPIO read and write test succesful

--- Tools/AP_Periph/bldc_motor.cpp
+++ Tools/AP_Periph/bldc_motor.cpp
@@ -10,7 +10,7 @@ extern AP_Periph_FW periph;
 
 SinglePhaseBLDC::SinglePhaseBLDC()
 {
-    //init code
+    // crashses here all time time hal.gpio->pinMode(GPIO_DEADMAN_BUTTON, HAL_GPIO_INPUT);
 }
 
 void SinglePhaseBLDC::update()
@@ -18,14 +18,15 @@ void SinglePhaseBLDC::update()
     static uint32_t loop_counter = 0;
 
     loop_counter++;
+    bool button_pressed = (hal.gpio->read(GPIO_DEADMAN_BUTTON) == 0);
 
-    if(loop_counter % 2000 > 1000)//toggle each second
+    if(loop_counter % 1000 > 100)//toggle each second
     {
-        hal.gpio->write(GPIO_TEST_LED, 0);
+        hal.gpio->write(GPIO_TEST_LED, button_pressed);
     }
     else
     {
-        hal.gpio->write(GPIO_TEST_LED, 1);
+        hal.gpio->write(GPIO_TEST_LED, !button_pressed);
     }
 }
 
