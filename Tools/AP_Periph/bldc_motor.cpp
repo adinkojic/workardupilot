47a7621a60bbfa579ad9333201a8c5096eb4eab2
Adin Kojic <adin.kojic@stratospheres.net>
2026-02-25 09:31:59 -0500

    led now flashes at 2Hz

--- Tools/AP_Periph/bldc_motor.cpp
+++ Tools/AP_Periph/bldc_motor.cpp
@@ -7,14 +7,26 @@
 extern const AP_HAL::HAL &hal;
 extern AP_Periph_FW periph;
 
+
 SinglePhaseBLDC::SinglePhaseBLDC()
 {
-    //constructor
+    //init code
 }
 
 void SinglePhaseBLDC::update()
 {
-    //called every loop
+    static uint32_t loop_counter = 0;
+
+    loop_counter++;
+
+    if(loop_counter % 2000 > 1000)//toggle each second
+    {
+        hal.gpio->write(GPIO_TEST_LED, 0);
+    }
+    else
+    {
+        hal.gpio->write(GPIO_TEST_LED, 1);
+    }
 }
 
 #endif
